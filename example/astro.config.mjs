297c84d998eb1fca6411d946780735b74ddc526a
casoon <joern.seidel@casoon.de>
2025-04-11 12:31:00 +0200

    update

--- example/astro.config.mjs
+++ example/astro.config.mjs
@@ -4,5 +4,14 @@ import alpine from '@astrojs/alpinejs';
 export default defineConfig({
   output: 'static',
   outDir: '../docs',
-  integrations: [alpine()]
+  integrations: [alpine()],
+  vite: {
+    resolve: {
+      alias: {
+        'casoon-ui-lib/base.css': 'casoon-ui-lib/base.css'
+      }
+    }
+  }
 });
+
+//    "casoon-ui-lib": "github:casoon/casoon-ui-lib",
